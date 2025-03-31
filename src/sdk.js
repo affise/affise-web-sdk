5c119a164a9d9f451fd60bf4881a798be369a8c6
Sergei Semiannikov <s.semiannikov@affise.com>
2025-03-31 13:38:23 +0300

    conversion

--- src/sdk.js
+++ src/sdk.js
@@ -43,6 +43,72 @@ export default class AffiseSDK {
         }
     }
 
+    getAdvertiserTransactionId(advertiserId) {
+        let tid = this._fetch(`aff_tid_c_a_${advertiserId}`).split("|");
+        if (tid) {
+            tid = tid[tid.length - 1];
+        } else {
+            tid = this._fetch(`aff_tid_i_a_${advertiserId}`);
+        }
+        return tid;
+    }
+
+    getTransactionId(offerId) {
+        let tid = this._fetch(`aff_tid_c_o_${offerId}`).split("|");
+        if (tid) {
+            tid = tid[tid.length - 1];
+        } else {
+            tid = this._fetch(`aff_tid_i_o_${offerId}`);
+        }
+        // Fallback for previous cookies when we did not have advertiser and offer cookies
+        if (!tid) {
+            tid = this._fetch(`aff_tid_${offerId}`);
+        }
+        return tid;
+    }
+
+    _getDefaultOrganicClickOptions() {
+        let sub1 = "";
+
+        if (this.urlParameter('fbclid')) {
+            sub1 = "Facebook";
+            if (this.urlParameter('fbclid').slice(0, 3) === "PAA") {
+                sub1 = "Instagram";
+            }
+        }
+
+        if (this.urlParameter('gclid')) {
+            sub1 = "Google"
+        }
+
+        if (this.urlParameter('ttclid')) {
+            sub1 = "Tiktok"
+        }
+
+        if (this.urlParameter('MSCLKID') || this.urlParameter('msclkid')) {
+            sub1 = "Microsoft"
+        }
+
+        if (this.urlParameter('OutbrainClickId')) {
+            sub1 = "Outbrain"
+        }
+
+        if (this.urlParameter('TCLID')) {
+            sub1 = "Taboola"
+        }
+
+        return {
+            sub1: this.urlParameter('sub1') || sub1,
+            sub2: this.urlParameter('sub2') || document.referrer,
+            sub3: this.urlParameter('sub3') || '/' + window.location.pathname.split('/')[1],
+            sub4: this.urlParameter('sub4') || window.location.pathname,
+            sub5: this.urlParameter('sub5') || window.location.search,
+            source_id: this.urlParameter('source_id') || "organic",
+            transaction_id: this.urlParameter('_aff_transaction_id'),
+            organic: 1
+        }
+    }
+
     click(options) {
         if (options.do_not_track === true) {
             return Promise.resolve("");
@@ -63,7 +129,7 @@ export default class AffiseSDK {
 
                 const url = new URL(`${trackingDomain}/sdk/click`)
 
-                const queryParams = new URLSearchParams(url.search + '&format=json&websdk=1');
+                const queryParams = new URLSearchParams(url.search);
 
                 for (const k in customParams) {
                     if (customParams.hasOwnProperty(k)) {
@@ -72,9 +138,9 @@ export default class AffiseSDK {
                 }
 
                 queryParams.set('affid', options.affiliate_id || '');
-                queryParams.set('afoffer_id', options.offer_id || '');
 
                 queryParams.set('afclick', options.click || '');
+                queryParams.set('afoffer_id', options.offer_id || '');
                 queryParams.set('affgoal', options.goal || '');
                 queryParams.set('afstatus', options.status || '');
                 queryParams.set('afcurrency', options.currency || '');
@@ -82,6 +148,11 @@ export default class AffiseSDK {
                 queryParams.set('afsecure', options.secure || '');
                 queryParams.set('afpromo_code', options.promo_code || '');
                 queryParams.set('afuser_id', options.user_id || '');
+                queryParams.set('afprice', options.price || '');
+                queryParams.set('pf_order_id', options.pf_order_id || '');
+                queryParams.set('pf_sku', options.pf_sku || '');
+                queryParams.set('pf_quantity', options.pf_quantity || '');
+                queryParams.set('pf_price', options.pf_price || '');
 
                 queryParams.set('async', 'json')
 
@@ -132,6 +203,172 @@ export default class AffiseSDK {
         });
     }
 
+    conversion(options) {
+        if (!options.transaction_id) {
+            if (this._isDefined(options.offer_id)) {
+                options.transaction_id = this._fetch(`aff_tid_c_o_${options.offer_id}`);
+                if (!options.transaction_id) {
+                    options.transaction_id = this._fetch(`aff_tid_i_o_${options.offer_id}`);
+                }
+                // Fallback for previous cookies when we did not have advertiser and offer cookies
+                if (!options.transaction_id) {
+                    options.transaction_id = this._fetch(`aff_tid_${options.offer_id}`);
+                }
+            }
+        }
+
+        if (options.transaction_id && options.transaction_id.length > 332) {
+            options.transaction_id = options.transaction_id.substring(0, 33) + options.transaction_id.substring(options.transaction_id.length - 297, options.transaction_id.length);
+        }
+
+        return new Promise((resolve, reject) => {
+            this._getCustomParams().then((customParams) => {
+                const trackingDomain = this._isDefined(options.tracking_domain) ? options.tracking_domain : this._trackingDomain;
+
+                const url = new URL(`${trackingDomain}/success.jpg`)
+
+                const queryParams = new URLSearchParams(url.search + '&success=1');
+
+                for (const k in customParams) {
+                    if (customParams.hasOwnProperty(k)) {
+                        queryParams.set(k, customParams[k])
+                    }
+                }
+
+                queryParams.set('transaction_id', options.transaction_id || '');
+                queryParams.set('event_id', options.event_id || 0);
+
+                if (this._isDefined(options.offer_id)) {
+                    queryParams.set('afoffer_id', options.offer_id);
+                }
+
+                if (this._isDefined(options.affiliate_id)) {
+                    queryParams.set('affid', options.affiliate_id);
+                }
+
+                if (this._isDefined(options.advertiser_id)) {
+                    queryParams.set('afadv_id', options.advertiser_id);
+                }
+
+                if (this._isDefined(options.promo_code)) {
+                    queryParams.set('promo_code', options.promo_code);
+                }
+
+                if (this._isDefined(options.amount)) {
+                    queryParams.set('amount', options.amount);
+                }
+
+                if (this._isDefined(options.adv1)) {
+                    queryParams.set('adv1', options.adv1)
+                }
+
+                if (this._isDefined(options.adv2)) {
+                    queryParams.set('adv2', options.adv2)
+                }
+
+                if (this._isDefined(options.adv3)) {
+                    queryParams.set('adv3', options.adv3)
+                }
+
+                if (this._isDefined(options.adv4)) {
+                    queryParams.set('adv4', options.adv4)
+                }
+
+                if (this._isDefined(options.adv5)) {
+                    queryParams.set('adv5', options.adv5)
+                }
+
+                if (this._isDefined(options.sub1)) {
+                    queryParams.set('sub1', options.sub1)
+                }
+
+                if (this._isDefined(options.sub2)) {
+                    queryParams.set('sub2', options.sub2)
+                }
+
+                if (this._isDefined(options.sub3)) {
+                    queryParams.set('sub3', options.sub3)
+                }
+
+                if (this._isDefined(options.sub4)) {
+                    queryParams.set('sub4', options.sub4)
+                }
+
+                if (this._isDefined(options.sub5)) {
+                    queryParams.set('sub5', options.sub5)
+                }
+
+                if (this._isDefined(options.order_id)) {
+                    queryParams.set('order_id', options.order_id)
+                }
+
+                if (this._isDefined(options.verification_token)) {
+                    queryParams.set('verification_token', options.verification_token)
+                }
+
+                if (this._isDefined(options.email)) {
+                    queryParams.set('email', options.email)
+                }
+
+                if (this._isDefined(options.order)) {
+                    queryParams.set('order', JSON.stringify(options.order));
+                }
+
+                if (this._isDefined(options.user_id)) {
+                    queryParams.set('user_id', options.user_id);
+                }
+
+                if (options.disable_fingerprinting === true) {
+                    queryParams.delete('asdkfp');
+                }
+
+                if (this._isDefined(options.parameters)) {
+                    Object.keys(options.parameters).forEach(p => queryParams.set(p, options.parameters[p]));
+                }
+
+                queryParams.set('event_source_url', window.location.hostname)
+
+                url.search = queryParams.toString();
+
+                fetch(url.toString(), {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json'
+                    },
+                    credentials: 'include'
+                })
+                    .then((response) => {
+                        if (response.status === 200) {
+                            return response.json();
+                        } else {
+                            return { conversion_id: '', transaction_id: '', html_pixel: '' };
+                        }
+                    })
+                    .then((response) => {
+                        if (response.html_pixel != '') {
+                            const script = document.createElement('iframe');
+                            script.width = 1;
+                            script.height = 1;
+                            script.frameBorder = 0;
+
+                            const src = "https://" + trackingDomain + "/success.jpg" + queryParams + "&success=1";
+                            script.setAttribute("src", src)
+
+                            document.getElementsByTagName('body')[0].appendChild(script);
+
+                            script.contentWindow.document.open();
+                            script.contentWindow.document.close();
+                        }
+                        resolve({ transaction_id: response.transaction_id, conversion_id: response.conversion_id });
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        resolve({ conversion_id: '', transaction_id: '', html_pixel: '' });
+                    })
+            });
+        });
+    }
+
     _getCustomParams() {
         return Promise.all([
             this.customParamProvider,
@@ -170,6 +407,13 @@ export default class AffiseSDK {
 
     _isDefined(value) {
         return typeof value !== 'undefined' && value !== undefined && value !== null;
+    }
+
+    _setDefaultFromURL(queryParams, paramName) {
+        const paramValue = this.urlParameter(paramName);
+        if (this._isDefined(paramValue)) {
+            queryParams.set(paramName, paramValue);
+        }
     }
 
     urlParameter(name) {
